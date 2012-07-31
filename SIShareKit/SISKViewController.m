b99dbd9a8b28daa58d7c62ae46dcc3b230dceb4d
Xiao ChenYu <yunsn0303@gmail.com>
2012-08-01 00:24:03 +0800

    修改demo

--- SIShareKit/SISKViewController.m
+++ SIShareKit/SISKViewController.m
@@ -14,95 +14,217 @@
 //#import "SISKQQWeiBoDialogView.h"
 #import "SISKQQSendView.h"
 
-@interface SISKViewController () <OpenApiDelegate>
+
+@interface SISKViewController ()<SISKTXWeiboEngineDelegate, SISKQQSendViewDelegate>
 
 @end
 
 @implementation SISKViewController
 @synthesize textViewMessage = _textViewMessage;
-@synthesize imageViewSend = _imageViewSend;
+@synthesize imageViewSend   = _imageViewSend;
 
-- (void)viewDidLoad
+-(void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    // Do any additional setup after loading the view, typically from a nib.
     _textViewMessage.delegate = self;
-    
 }
 
-- (void)viewDidUnload
+-(void)viewDidUnload
 {
-    [self setTextViewMessage:nil];
-    [self setImageViewSend:nil];
+    [self setTextViewMessage: nil];
+    [self setImageViewSend: nil];
     [super viewDidUnload];
     // Release any retained subviews of the main view.
 }
 
-- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+-(BOOL)shouldAutorotateToInterfaceOrientation: (UIInterfaceOrientation) interfaceOrientation
 {
-    return (interfaceOrientation != UIInterfaceOrientationPortraitUpsideDown);
+    return(interfaceOrientation != UIInterfaceOrientationPortraitUpsideDown);
 }
 
 #pragma mark - UITextViewDelegate
-- (BOOL)textView:(UITextView *)textView shouldChangeTextInRange:(NSRange)range replacementText:(NSString *)text
+-(BOOL)textView: (UITextView *) textView shouldChangeTextInRange: (NSRange) range replacementText: (NSString *) text
 {
-    if ([text isEqualToString:@"\n"]) {
+    if ([text isEqualToString: @"\n"]) {
         [textView resignFirstResponder];
-        return NO;
+        return(NO);
     }
-    return YES;
+    return(YES);
 }
 
-#pragma mark - tx button action 
-- (IBAction)txLoginAction:(id)sender {
+#pragma mark - tx button action
+-(IBAction)txLoginAction: (id) sender
+{
     SISKTXWeiboEngine *engine = [SISKTXWeiboEngine sharedSISKTXWeiboEngine];
+    engine.delegate = self;
+
     if (![engine isSessionValid]) {
         [engine showLogin];
     }
-    
 }
 
-- (IBAction)txLogoutAction:(id)sender {
+-(IBAction)txLogoutAction: (id) sender
+{
     SISKTXWeiboEngine *engine = [SISKTXWeiboEngine sharedSISKTXWeiboEngine];
+    engine.delegate = self;
+
     if ([engine isSessionValid]) {
         [engine logout];
     }
 }
 
-- (IBAction)txSendAction:(id)sender {
+-(IBAction)txSendAction: (id) sender
+{
     SISKTXWeiboEngine *engine = [SISKTXWeiboEngine sharedSISKTXWeiboEngine];
-    engine.openApi.delegate = self;
-    if (![engine isSessionValid]) {
+    engine.delegate = self;
+    
+    if ([engine isSessionValid]) {
+        SISKQQSendView *sendview = [[SISKQQSendView alloc] initWithText:_textViewMessage.text image:_imageViewSend.image];
+        sendview.qqSendDelegate = self;
+        [sendview show:YES];
+//        [engine sendPublishWeiboWithUIImage:_imageViewSend.image weiboContent:_textViewMessage.text];
+    }else{
         [engine showLogin];
-    }else {
-        NSString *filePath = [[NSBundle mainBundle] pathForResource:@"mm" ofType:@"jpg"];
-        [engine sendPublishWeiBoWithImage:filePath weiboContent:_textViewMessage.text];
     }
+    
 }
 
-- (IBAction)testViewAction:(id)sender {
-
-}
-
-- (IBAction)mmSendAction:(id)sender {
-
-}
-
-
-#pragma mark - OpenApidelegate
--(void)sendSuccess:(OpenApi *)openApi
+-(IBAction)testViewAction: (id) sender
 {
-    NSLog(@"success");
 }
 
--(void)sendFaile:(OpenApi *)openApi
+-(IBAction)mmSendAction: (id) sender
 {
-    NSLog(@"faile");
 }
 
-- (void)sendInterfaceFaile:(OpenApi *)openApi
+
+#pragma mark - SISKTXWeiboEngineDelegate
+// 登录成功
+-(void)oauthDidSuccess: (SISKTXWeiboEngine *) engine
 {
-    NSLog(@"interface faile");
+    UIAlertView *alertView = [[UIAlertView alloc] initWithTitle: @"登录"
+                                                        message: @"登录成功"
+                                                       delegate: nil
+                                              cancelButtonTitle: @"OK"
+                                              otherButtonTitles: nil];
+
+    [alertView show];
+}
+
+// 登录失败
+-(void)oauthDidFailed: (SISKTXWeiboEngine *) engine withErrorNumber: (NSInteger) errorNumber
+{
+    UIAlertView *alertView = [[UIAlertView alloc] initWithTitle: @"登录"
+                                                        message: @"登录失败"
+                                                       delegate: nil
+                                              cancelButtonTitle: @"OK"
+                                              otherButtonTitles: nil];
+    
+    [alertView show];
+}
+
+// 拒绝授权
+-(void)userRefuseAuthorize: (SISKTXWeiboEngine *) engine
+{
+    UIAlertView *alertView = [[UIAlertView alloc] initWithTitle: @"登录"
+                                                        message: @"拒绝授权"
+                                                       delegate: nil
+                                              cancelButtonTitle: @"OK"
+                                              otherButtonTitles: nil];
+    
+    [alertView show];
+}
+
+// 发送成功
+-(void)sendSuccess: (SISKTXWeiboEngine *) engine;
+{
+    UIAlertView *alertView = [[UIAlertView alloc] initWithTitle: @"发送成功"
+                                                        message: @"发送成功"
+                                                       delegate: nil
+                                              cancelButtonTitle: @"OK"
+                                              otherButtonTitles: nil];
+    
+    [alertView show];
+}
+
+// 发送失败
+-(void)sendFailed: (SISKTXWeiboEngine *) engine;
+{
+    UIAlertView *alertView = [[UIAlertView alloc] initWithTitle: @"发送失败"
+                                                        message: @"发送失败"
+                                                       delegate: nil
+                                              cancelButtonTitle: @"OK"
+                                              otherButtonTitles: nil];
+    
+    [alertView show];
+}
+
+// 调用接口失败
+-(void)sendInterfaceFaile: (SISKTXWeiboEngine *) engine
+{
+    UIAlertView *alertView = [[UIAlertView alloc] initWithTitle: @"调用接口失败"
+                                                        message: @"调用接口失败"
+                                                       delegate: nil
+                                              cancelButtonTitle: @"OK"
+                                              otherButtonTitles: nil];
+    
+    [alertView show];
+}
+
+// 验证失败
+-(void)authorizationFailedOrDidNotAuthorize: (SISKTXWeiboEngine *) engine
+{
+    UIAlertView *alertView = [[UIAlertView alloc] initWithTitle: @"验证失败"
+                                                        message: @"验证失败"
+                                                       delegate: nil
+                                              cancelButtonTitle: @"OK"
+                                              otherButtonTitles: nil];
+    
+    [alertView show];
+}
+
+// 调用成功
+-(void)callFunctionSuccess: (SISKTXWeiboEngine *) engine withResult: (NSString *) resultString
+{
+    
+}
+
+// 调用失败
+-(void)callFunctionFailed: (SISKTXWeiboEngine *) engine
+{
+    
+}
+
+// 获取用户信息时候调用接口失败
+-(void)callTheInterfaceFailed: (SISKTXWeiboEngine *) engine
+{
+    
+}
+
+// 登出完成
+-(void)logoutDidSuccess: (SISKTXWeiboEngine *) engine
+{
+    UIAlertView *alertView = [[UIAlertView alloc] initWithTitle: @"登出完成"
+                                                        message: @"登出完成"
+                                                       delegate: nil
+                                              cancelButtonTitle: @"OK"
+                                              otherButtonTitles: nil];
+    
+    [alertView show];
+}
+
+
+#pragma mark - SISKQQSendViewDelegate
+- (void)buttonActionWillSendWeibo:(SISKQQSendView *)sendView
+                        withImage:(UIImage *)image
+                       andMessage:(NSString *)message
+{
+        SISKTXWeiboEngine *engine = [SISKTXWeiboEngine sharedSISKTXWeiboEngine];
+    engine.delegate = self;
+
+    [engine sendPublishWeiboWithUIImage:image weiboContent:message];
+    [sendView hide:YES];
 }
 
 @end
